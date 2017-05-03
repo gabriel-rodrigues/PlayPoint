aed5c25a3810cc57d9d7debe6ecb91addac9db83
gabriel.rodrigues <gabriel.rodrigues@apesoftware.com.br>
2017-05-03 20:29:22 -0300

    item

---
+++ PlayPoint/EventoTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  EventoTableViewCell.swift
+//  PlayPoint
+//
+//  Created by Gabriel Rodrigues on 03/05/17.
+//  Copyright © 2017 Iesb. All rights reserved.
+//
+
+import UIKit
+
+internal class EventoTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var nomeEnvetoLabel: UILabel!
+    @IBOutlet weak var localLabel: UILabel!
+    @IBOutlet weak var dataHoraLabel: UILabel!
+    @IBOutlet weak var fotoImavem: UIImageView!
+    
+}
---
+++ PlayPoint/EventosViewController.swift
@@ -0,0 +1,54 @@
+//
+//  EventosViewController.swift
+//  PlayPoint
+//
+//  Created by Gabriel Rodrigues on 03/05/17.
+//  Copyright © 2017 Iesb. All rights reserved.
+//
+
+import UIKit
+
+class EventosViewController: UIViewController {
+
+    public let cellIdentifier = "celulaEvento"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension EventosViewController : UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celula = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath) as! EventoTableViewCell
+        
+        celula.nomeEnvetoLabel.text = "TESTANDO A BAGACEIRA ESSA POHODA TODA VALENDO VAMOS VER SE VAI DAR BOM ESSA TELA POR FAVOR"
+        
+        return celula
+    }
+}
